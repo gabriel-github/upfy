8f59b5dee84d8557bf2bcc6978579963b36b3a08
gabriel-github <gabrielferrsantos201@gmail.com>
2021-08-23 11:20:08 -0300

    solved challenge

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,25 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        bg="pGray.800"
+        maxW={['300px', '500px', '900px']}
+        maxH={['350px', '450px', '6000px']}
+      >
+        <ModalBody maxW="100%">
+          <Image src={imgUrl} />
+        </ModalBody>
+
+        <ModalFooter bg="pGray.800" h="2rem" py="20px" borderBottomRadius="5px">
+          <Link href={imgUrl}>Abrir original</Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
